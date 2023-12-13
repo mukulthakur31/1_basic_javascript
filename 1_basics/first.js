f1b92f8ecf13e132382d6c46181023fb4d367b10
mukulthakur31 <130899371+mukulthakur31@users.noreply.github.com>
2023-12-13 04:40:17 +0000

    second commit

--- 1_basics/first.js
+++ 1_basics/first.js
@@ -1,1 +1,9 @@
-console.log("mukul")
+// in javascript datatypes are classified into two types primitive and non primitive 
+//primitive are just like call by value and non primitive are just like call ny reference 
+//in primitive you dont get the refrences of the values you only get the values in primitive there are 7 datatypes =>number,string,boolean,null,undefine d,symbol,bigint
+//REference (non primitive )
+//isme hme memory ke refrences milte ha 
+//Array,Objects,Functions
+
+//agr hm kisi variable  ko null define krte ha  to uska datatype null nhi object aayega 
+//hamare jitne bhi non premitive datatypes ha unka return type object hi aata ha 
